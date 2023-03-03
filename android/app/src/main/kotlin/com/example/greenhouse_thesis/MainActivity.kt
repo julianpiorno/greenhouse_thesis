6dfd8221f16302eca8c527009bfdbeca84dd4fad
Julian Piorno <julian.piorno@outlook.com>
2023-03-03 09:54:07 +0100

    Initilize

---
+++ android/app/src/main/kotlin/com/example/greenhouse_thesis/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.greenhouse_thesis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(GreenhouseApp());
+
+class GreenhouseApp extends StatelessWidget {
+  const GreenhouseApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Greenhouse App",
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Greenhouse App"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Text("Hello world!"),
+          
+        ],
+      ),
+    );
+  }
+}
+
+Widget body(){
+  return Container(
+    decoration: BoxDecoration(
+      image: DecorationImage(image: NetworkImage("http://www.villarino.gob.ar/wp-content/uploads/2020/10/UNQ-deuda.jpg"))
+    ),
+  )
+}
